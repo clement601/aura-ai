d6e53a3534876cc65f596a42effde80963f3a3c7
Clément Aoustin <aoustinc49@gmail.com>
2026-01-20 21:15:54 +0100

    Première version Aura AI

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        aura: {
+          dark: '#030712',
+          purple: '#a855f7',
+          indigo: '#6366f1',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
